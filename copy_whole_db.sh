db86bac070044d70ccf437007bbcc73211df5e38
Lance R. Vick <lance@lrvick.net>
2015-04-28 08:58:43 +0000

    overhaul copy_whole_db.sh script to always confirm, no longer require tempfiles, and clean up after itself

--- copy_whole_db.sh
+++ copy_whole_db.sh
@@ -1,44 +1,65 @@
+#!/bin/bash
+
 if [ "x$6" == "x" ]; then
-  echo "Usage: ./copy_whole_db.sh sourceuser sourcepass sourcedb destuser destpass destdb"
+  echo "Usage: ./copy_whole_db.sh SOURCE_USER SOURCE_PASS SOURCE_DB DEST_USER DEST_PASS DEST_DB"
   exit
 fi
 
-if [ "x$1" == "x--check" ]; then
-   check=true
-   shift
-fi
+SOURCE_USER=$1
+SOURCE_PASS=$2
+SOURCE_DB=$3
+DEST_USER=$4
+DEST_PASS=$5
+DEST_DB=$6
 
-sourceuser=$1
-sourcepass=$2
-sourcedb=$3
-destuser=$4
-destpass=$5
-destdb=$6
+COLLECTIONS=$(echo "show collections" \
+    | mongo $SOURCE_DB -u $SOURCE_USER -p $SOURCE_PASS --quiet \
+    | grep -v "^system\." )
 
+SOURCE_COUNTS=$( column -t <( for COLLECTION in ${COLLECTIONS}; do
+    echo "print(db.${COLLECTION}.count(), \"\t${COLLECTION}\")" 
+done | mongo $SOURCE_DB -u $SOURCE_USER -p $SOURCE_PASS --quiet ))
 
-if [ "$check" == "true" ]; then
-  collections=`echo "show collections" | mongo $sourcedb -u $sourceuser -p $sourcepass --quiet | grep -v "^system\."`
-  echo mongo $sourcedb -u $sourceuser -p $sourcepass --quiet 
-  for i in $collections; do
-    echo "print(db.$i.count(), \"\t$i\")"
-  done | mongo $sourcedb -u $sourceuser -p $sourcepass --quiet > tmp_a
-  echo mongo $destdb -u $destuser -p $destpass --quiet
-  for i in $collections; do
-    echo "print(db.$i.count(), \"\t$i\")"
-  done | mongo $destdb -u $destuser -p $destpass --quiet > tmp_b
-  diff tmp_a tmp_b
+DEST_COUNTS=$( column -t <( for COLLECTION in ${COLLECTIONS}; do
+    echo "print(db.${COLLECTION}.count(), \"\t${COLLECTION}\")" 
+done | mongo $DEST_DB -u $DEST_USER -p $DEST_PASS --quiet ))
 
-else
-  mkdir -p log
-  echo "mongo $destdb -u $destuser -p $destpass"
-  read
-  echo "db.dropDatabase()" | mongo $destdb -u $destuser -p $destpass
-  echo "show collections" | mongo $sourcedb -u $sourceuser -p $sourcepass --quiet | grep -v "^system\." | while read collection; do
-    ./copy_collection.py \
-      --source $sourceuser:$sourcepass@$sourcedb/$collection \
-      --dest   $destuser:$destpass@$destdb/$collection \
-       > log/$collection.log 2>&1 &
-  done
-  sleep 5
-  grep Traceback log/*
-fi
+# Output table comparing SOURCE and DESTINATION collection counts
+printf "\nSOURCE: $SOURCE_DB"
+printf "\n\nDESTINATION: $DEST_DB\n\n"
+printf '%-37s | %-38s\n' "--- SOURCE ---" "--- DESTINATION ---"
+diff -y --width=80 \
+    <(printf %s "$SOURCE_COUNTS") \
+    <(printf %s "$DEST_COUNTS")
+
+# Confirm with user before proceeding
+printf "\n\n"
+read -p "Sync SOURCE to DESTINATION (y/n)? " choice
+case "$choice" in 
+  n|N ) echo "Aborting sync."; exit;;
+  y|Y ) echo "Starting sync...";;
+  * ) echo "invalid";;
+esac
+
+# Start sync/tail processes for each collection
+PIDS=()
+for COLLECTION in $COLLECTIONS; do
+    echo ./copy_collection.py \
+      --source $SOURCE_USER:$SOURCE_PASS@$SOURCE_DB/$COLLECTION \
+      --dest   $DEST_USER:$DEST_PASS@$DEST_DB/$COLLECTION
+    while true; do sleep 1;done &
+    PIDS=("${PIDS[@]}" "$!")
+done
+
+# Kill all sync processes and exit on <CTRL+C>
+trap ctrl_c INT
+function ctrl_c() {
+    echo "Caught <Ctrl-C>. Exiting..."
+    for PID in ${PIDS[@]}; do
+        kill $PID
+    done
+    exit
+}
+
+# Sleep Forever
+while true; do sleep 1; done
